14c5f6b37ca616793d097de1706d80d2838c9cae
pamuba <pamuba@users.noreply.github.com>
2020-07-17 21:57:15 +0530

    2nd

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
+import axios from 'axios'
 
 export default class CreateExercises extends Component {
     constructor(props) {
@@ -24,10 +25,15 @@ export default class CreateExercises extends Component {
 
     //called right before anything is displayed on the page
     componentDidMount(){
-        this.setState({
-            users:['test user'],
-            username: 'test user'
-        })
+       axios.get("http://localhost:5000/users/")
+            .then(response => {
+                if(response.data.length > 0){
+                    this.setState({
+                        users: response.data.map(user => user.username),
+                        username: response.data[0].username
+                    })
+                }
+            })
     }
 
     onChangeUsername(e){
@@ -63,7 +69,10 @@ export default class CreateExercises extends Component {
             duration: this.state.duration,
             date: this.state.date
           }
-          console.log(exercise)
+        //   console.log(exercise)
+
+        axios.post('http://localhost:5000/exercises/add', exercise)
+        .then(res => console.log(res.data))
 
         //   window.location = '/'
       }
--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,45 +1,64 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from 'axios'
 
 export default class CreateUsers extends Component {
-    constructor(props) {
-        super()
+  constructor(props) {
+    super();
 
-        this.onChangeUsername = this.onChangeUsername.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 
-    
-        this.state = {
-             username:'',
-        }
-    }
+    this.state = {
+      username: "",
+    };
+  }
 
-    onChangeUsername(e){
-        this.setState({
-            username:e.target.value
-        });
-    }
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
 
-    onSubmit(e){
-        e.preventDefault();
+  onSubmit(e) {
+    e.preventDefault();
 
-        const exercise = {
-          username: this.state.username,
-          description: this.state.description,
-          duration: this.state.duration,
-          date: this.state.date
-        }
-        console.log(exercise)
+    const user = {
+      username: this.state.username,
+    };
+    console.log(user);
 
-      //   window.location = '/'
-    }
+    axios.post('http://localhost:5000/users/add', user)
+        .then(res => console.log(res.data))
 
-    render() {
-        return (
-            <div>
-                <p>You are in Create Users Component</p>
-            </div>
-        )
-    }
+    this.setState({
+      username: "",
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create User"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
-
-
